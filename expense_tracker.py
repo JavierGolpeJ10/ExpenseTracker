c57d073504989395073d2ccdb28daac9a99881da
JavierGolpeJ10 <golpeju2@uwm.edu>
2024-08-09 17:52:52 -0500

    Expense_tracker edits

---
+++ expense_tracker.py
@@ -0,0 +1,25 @@
+def main():
+    print(f"Running Expense Tracker!")
+    # Get user input to get the expense
+    get_user_input()
+    # then write it into a cvs file
+    save_expense_to_file()
+    #  read file and summarize expenses
+    summarize_expense()
+
+def get_user_input():
+    print("Getting User Expense!")
+
+
+def save_expense_to_file():
+    print("Savign User expense to file")
+
+
+def summarize_expense():
+    print("Summarizing User Expense")
+
+
+if __name__ == "__main__":
+    main()
+
+
